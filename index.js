62e41d8197208b8d02d04c8fb6edb7a00c97f5f7
MoadC <RappelzKloyster@gmail.com>
2021-07-13 00:26:39 +0100

    finished JS

--- index.js
+++ index.js
@@ -63,16 +63,45 @@ var items= [
 
 window.addEventListener('load',function(){
     console.log('loaded');
-
     
     //console.log(li[1]);
     function select(){
-
-        //let li = document.getElementById("item");
-        li.classList.toggle("selected");
+        this.classList.toggle("selected");
     }
     let li = document.getElementById("item");
     li.addEventListener('click', select);
+    let li2 = document.getElementById("item2");
+    li2.addEventListener('click', select);
+    let li3 = document.getElementById("item3");
+    li3.addEventListener('click', select);
+
+    const allLi = document.getElementsByTagName('li');
+    let lenLi = allLi.length;
+    for (let index = 0; index < lenLi; index++) {
+        let newli = allLi[index];  
+        console.log("item" + [index]);
+        let liId = "item" + [index];
+        newli.id = liId;
+        //console.log(newli.id);
+        newli.addEventListener('click', select);
+        
+    }
 
 });
+
+function total(){
+
+    let result = 0;
+    let results=document.getElementById('results');
+    //results.value='';
+    let chosen = document.getElementsByClassName('selected');
+    let len = chosen.length;
+    console.log(len);
+    for (let index = 0; index < len; index++) {       
+        result += chosen[index].value;
+    }
+    const text = results.innerText= "your total is : ";
+    results.value =text + result ;
+    result = 0;
+}
 
