44ef99ff2c1f1fe01717720b333bacce5564a7f1
MoadC <RappelzKloyster@gmail.com>
2021-07-14 18:32:22 +0100

    final touchs...

--- index.js
+++ index.js
@@ -86,9 +86,9 @@ span.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 });
